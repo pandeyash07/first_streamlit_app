50500557530a157ef1d0aa2b19e06a8a05b1622c
pandeyash07 <104776112+pandeyash07@users.noreply.github.com>
2022-05-02 22:25:11 +0530

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title("My Mom's new Healthy Dinner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3  & Blueberry Oatmeal')
-streamlit.text('kale, spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
