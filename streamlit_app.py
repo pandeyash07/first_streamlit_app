a890236f9f45790caa446e0e842331ddc5cd8d8a
pandeyash07 <104776112+pandeyash07@users.noreply.github.com>
2022-05-02 22:21:33 +0530

    Update streamlit_app.py
    ch

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My Mom's new Healthy Dinner")
 streamlit.header('Breakfast Menu')
 streamlit.text('omega 3  & Blueberry Oatmeal')
 streamlit.text('kale, spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range EGG')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
