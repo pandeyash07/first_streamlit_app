848dd05eda1e765eff8dc46e1b9973387d878f33
pandeyash07 <104776112+pandeyash07@users.noreply.github.com>
2022-05-06 15:13:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,13 @@ streamlit.dataframe(fruit_to_show)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 streamlit.text(fruityvice_response)
 
 streamlit.header('Fruity Fruit Advice!')
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+
 
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json()) 
 # output it the screen as a table
@@ -38,7 +38,6 @@ streamlit.header('Fruityvice Fruit Advice')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 
